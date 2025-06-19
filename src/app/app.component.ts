74eb32089592329f724d0ccd6e2d6b736698430a
RafaelGx <rafael_junio_moraes@hotmail.com>
2025-06-19 12:33:48 -0300

    feat: integrate Tailwind CSS and initial setup
    - Added Tailwind CSS dependencies to package.json.
    - Removed app.component.css and app.component.html files.
    - Updated app.component.ts to use an empty template.
    - Deleted app.component.spec.ts as it is no longer needed.
    - Updated global styles to import Tailwind CSS.
    - Created a .postcssrc.json configuration for Tailwind CSS.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [],
+  template: ` `,
 })
 export class AppComponent {
   title = '3d_Portifolio';
