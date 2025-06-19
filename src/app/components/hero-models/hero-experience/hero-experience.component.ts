2052b51c075c76123a878a15ac2b4f0b676abbd0
RafaelGx <rafael_junio_moraes@hotmail.com>
2025-06-19 15:43:34 -0300

    feat: add HeroExperience component and integrate into hero section

---
+++ src/app/components/hero-models/hero-experience/hero-experience.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-experience',
+  imports: [],
+  templateUrl: './hero-experience.component.html',
+  styles: ``
+})
+export class HeroExperienceComponent {
+
+}
--- src/app/components/sections/hero/hero.component.ts
+++ src/app/components/sections/hero/hero.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { words } from '../../../constants';
 import { ButtonComponent } from "../../button/button.component";
+import { HeroExperienceComponent } from '../../hero-models/hero-experience/hero-experience.component';
 
 @Component({
   selector: 'app-hero',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, HeroExperienceComponent],
   templateUrl: './hero.component.html',
 })
 export class HeroComponent {
