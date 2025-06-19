612fc5f3aeffed2e0e00db1e682f15cc19735b07
RafaelGx <rafael_junio_moraes@hotmail.com>
2025-06-19 13:19:12 -0300

    Add constants for all assets.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   imports: [],
-  template: ` `,
+  template: `<main>
+    <div>
+      <h1 class="text-3xl underline">Test</h1>
+    </div>
+  </main> `,
 })
 export class AppComponent {
   title = '3d_Portifolio';
